d2fa03390d721f2814ad804afd94915eb5730974
Edson Aguiar <edsonaguiar888@gmail.com>
2026-09-15 13:49:17 -0300

    alterado o nome do repositorio backend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🔓 Habilita requisições cross-origin do Angular
+  app.enableCors();
+
+
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
